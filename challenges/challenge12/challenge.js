2ef2566f13c4eb2bdc2c2ae55806e5806d5e30f0
Alex Prosser <apsoutherngaming07@gmail.com>
2020-12-19 15:33:17 -0600

    challenge 12

--- challenges/challenge12/challenge.js
+++ challenges/challenge12/challenge.js
@@ -2,5 +2,19 @@ const input = require('fs').readFileSync('./challenges/challenge12/input.txt').t
 const common = require('../../scripts/common');
 
 module.exports = () => {
-    return 0;
+    let instructions = input.split('\r\n');
+    let allFloors = [0];
+    let floor = 0;
+    let dir = 1;
+    
+    while (true) {
+        let tokens = instructions[floor].split(' ');
+        dir *= (tokens[0] == '0') ? -1 : 1;
+        floor += parseInt(tokens[1]) * dir;
+        allFloors.push(floor);
+        
+        if (instructions[floor] == null) break;
+    }
+    
+    return allFloors.length;
 }
